4acfb9e3a4c180a0b980dfa23c039b9488a53a4f
lazyspell <jeremy2975@yahoo.com>
2022-09-13 13:23:59 -0700

    styling update

--- profile_frontend/src/client/profile.client.js
+++ profile_frontend/src/client/profile.client.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
     uri: process.env.REACT_APP_Graphql_Link,
+    uri: "https://profile-backend-go-tpy8r.ondigitalocean.app/query",
     // uri: "http://localhost:8080/query",
 
     cache: new InMemoryCache(),
